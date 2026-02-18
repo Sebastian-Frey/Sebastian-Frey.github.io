767abe23901a0d004834dc05274e0cec8adef8a2
Sebastian Frey <133000173+Sebastian-Frey@users.noreply.github.com>
2026-02-18 19:39:23 +0100

    Remove GitHub Pages adapter from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import { githubPages } from "@astrojs/github-pages";
 
 export default defineConfig({
   site: "https://itssebastianfrey.com",
-
-  adapter: githubPages(),
-
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
